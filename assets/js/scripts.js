459542e6d5a8975a7b82a5cdcfe88560fca6d0f2
Даша Артюкевич <dartykevich@bk.ru>
2020-07-13 07:32:21 +0300

    Added scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,25 +1,44 @@
 const div = document.createElement('div')
-const divChild = document.createElement('divChild')
-const img = document.createElement('img')
+const divChild1 = document.createElement('div')
+const divChild2 = document.createElement('div')
+const divChild3 = document.createElement('div')
+const divChild4 = document.createElement('div')
+const divChild5 = document.createElement('div')
+const divChild6 = document.createElement('div')
+const divChild7 = document.createElement('div')
+const img1 = document.createElement('img')
+const img2 = document.createElement('img')
 const a = document.createElement('a')
 
-div.appendChild(div)
-div.appendChild(div)
-div.appendChild(div)
-div.appendChild(div)
-
+div.appendChild(divChild1)
+divChild1.appendChild(img1)
+div.appendChild(divChild2)
+div.appendChild(divChild3)
+div.appendChild(divChild4)
+divChild4.appendChild(divChild5)
+divChild5.appendChild(divChild6)
+divChild5.appendChild(divChild7)
+divChild6.appendChild(img2)
+divChild4.appendChild(a)
 
 
 console.log(div)
 
-h2.innerHTML = 'Lorem'
-p.innerHTML = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure, sunt itaque ducimus distinctio a ipsa. Maiores veritatis voluptatum quo dolor dolorum saepe dist. Nesciunt maiores consequatur sit iste non aliquid eum blanditiis fugit? Natus mollitia quos magni ex eveniet deserunt harum distinctio porro recusandae aliquam repellendus, ut aut? Commodi iure quos voluptatibus dignissimos nesciunt! Ratione, adipisci maiores distinctio dolores voluptas mollitia ducimus nulla deleniti cupiditate nobis! Odio dolorum doloremque a nobis incidunt numquam veritatis vel quibusdam harum magni animi vero illum aliquam voluptatum quod consequatur libero, repellendus quas nihil ratione similique suscipit minus ut iste? Vero, repellat voluptatem laudantium odio in autem magni omnis.'
-a.innerHTML = 'See more...'
+divChild1.innerHTML = "<img src='assets/images/place for photo.jpg'>"
+divChild2.innerHTML = 'Распространенные заблуждения об уходе за кошкой'
+divChild3.innerHTML = 'Некоторых животных природа призывает делать очень странные вещи: есть колбасные шкурки, нитки из покрывала, варенье и прочие, часто…'
+divChild7.innerHTML = '9 февраля 2018'
+a.innerHTML = 'Читать полностью >'
 
 div.classList.add('Post')
-h2.classList.add('Post__title')
-p.classList.add('Post__description')
-a.classList.add('Post__link')
+divChild1.classList.add('Post__img')
+divChild2.classList.add('Post__title')
+divChild3.classList.add('Post__text')
+divChild4.classList.add('Post__footer')
+divChild5.classList.add('Post__footer__date')
+divChild6.classList.add('Post__footer__date__img')
+divChild7.classList.add('Post__footer__date__text')
+a.classList.add('Post__footer__ink')
 
 a.href = '#'
 
